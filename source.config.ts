345752a5ab4455811627f06b824b25483cda028f
MISQZY <npershutov@gmail.com>
2026-05-19 02:34:45 +0300

    fix config import path error v2

--- source.config.ts
+++ source.config.ts
@@ -1,9 +1,6 @@
 import { defineDocs, defineConfig } from "fumadocs-mdx/config";
-import { SERVERS } from "@/config/servers";
 
-const dirs = Object.fromEntries(SERVERS.map((s) => [s.id, s.id]));
-
-export const duckburg = defineDocs({ dir: `content/${dirs.duckburg}` });
-export const duckhood = defineDocs({ dir: `content/${dirs.duckhood}` });
+export const duckburg = defineDocs({ dir: "content/duckburg" });
+export const duckhood = defineDocs({ dir: "content/duckhood" });
 
 export default defineConfig();
