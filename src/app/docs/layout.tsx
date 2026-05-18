1d8cff496366beec5655c71c34ca2ff91ed28334
MISQZY <npershutov@gmail.com>
2026-05-19 00:49:19 +0300

    fix docs container margin

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,14 +1,11 @@
-import Link from "next/link";
-
-// Shared wrapper around all /docs/* pages
 export default function DocsRootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen">
-      <div className="pt-12">{children}</div>
+    <div className="min-h-screen flex">
+      {children}
     </div>
   );
-}
+} 
