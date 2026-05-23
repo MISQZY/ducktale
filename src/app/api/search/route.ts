beeb8706b5061c17b8c4c992e0a7154edec28303
MISQZY <npershutov@gmail.com>
2026-05-23 22:01:06 +0300

    docsSource page Type cast

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,6 +1,7 @@
 import { docsSources } from "@/lib/source";
 import { SERVERS } from "@/config/servers";
 import { createSearchAPI } from "fumadocs-core/search/server";
+import { Page, PageData } from "fumadocs-core/source";
 
 export const { GET } = createSearchAPI("advanced", {
   tokenizer: {
@@ -8,7 +9,7 @@ export const { GET } = createSearchAPI("advanced", {
     normalizationCache: new Map(),
   },
   indexes: SERVERS.flatMap((s) =>
-    docsSources[s.id].getPages().map((page) => ({
+    docsSources[s.id].getPages().map((page: Page<PageData>) => ({
       title: page.data.title ?? "",
       description: page.data.description ?? "",
       url: page.url,
